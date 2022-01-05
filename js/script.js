598e4929d883d4bec9cc193a32ff7e570fe75778
Tharusha Induwara <tharusha.induwar@gmail.com>
2022-01-05 20:32:06 +0530

    Done coding part of navigation bar

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 console.log("Script Loaded");
+var menu1 = new Image;
+menu1.src=("assets/shared/icon-hamburger.svg");
+var menu2 = new Image;
+menu2.src=("assets/shared/icon-close.svg");
+
+var navLinks = document.getElementById("nav-links");
+var hamMenu = document.getElementById("toggle-ham-menu");
+var hamMenuIcon = document.getElementById("toggle-ham-menu").appendChild(menu1);
+
+function imgClick(){
+    console.log("HI");
+    if (hamMenuIcon.src.match("assets/shared/icon-hamburger.svg")){
+        hamMenuIcon.src = "assets/shared/icon-close.svg";
+        navLinks.classList.add("navbar-links-active");
+    }
+    else{
+        hamMenuIcon.src = "assets/shared/icon-hamburger.svg";
+        navLinks.classList.remove("navbar-links-active");
+    }
+    
+}
