c5d7d71d618668bcdf6b1bb7315844aa0cac076c
Tharusha Induwara <tharusha.induwar@gmail.com>
2022-01-10 08:21:54 +0530

    Beginning coding on crew page

--- js/script.js
+++ js/script.js
@@ -57,7 +57,7 @@ var moonDistance = document.getElementById("dis-value");
 var estTravel = document.getElementById("time-value");
 
 moonBtn.onclick = function(){
-    planet.src="assets/destination/image-moon.png";
+    planet.src="assets/destination/image-moon.webp";
     moonName.innerHTML = "MOON";
     moonDesc.innerHTML = "See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.";
     moonDistance.innerHTML = "384,400 km";
@@ -69,7 +69,7 @@ moonBtn.onclick = function(){
 
 }
 marsBtn.onclick = function(){
-    planet.src="assets/destination/image-mars.png";
+    planet.src="assets/destination/image-mars.webp";
     moonName.innerHTML = "MARS";
     moonDesc.innerHTML = "Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!";
     moonDistance.innerHTML = "225 MIL. km";
@@ -81,7 +81,7 @@ marsBtn.onclick = function(){
     titanBtn.classList.remove("planet-navbar-active");
 }
 europaBtn.onclick = function(){
-    planet.src="assets/destination/image-europa.png";
+    planet.src="assets/destination/image-europa.webp";
     moonName.innerHTML = "EUROPA";
     moonDesc.innerHTML = "The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.";
     moonDistance.innerHTML = "628 MIL. km";
@@ -93,7 +93,7 @@ europaBtn.onclick = function(){
     titanBtn.classList.remove("planet-navbar-active");
 }
 titanBtn.onclick = function(){
-    planet.src="assets/destination/image-titan.png";
+    planet.src="assets/destination/image-titan.webp";
     moonName.innerHTML = "TITAN";
     moonDesc.innerHTML = "The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.";
     moonDistance.innerHTML = "1.6 BIL. km";
